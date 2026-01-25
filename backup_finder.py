a3409f8cce2ff8cea3b8b7b1857b5270f7994254
asisamko <saulostyle.sk@gmail.com>
2026-01-25 16:05:10 +0100

    added support for macOS

--- backup_finder.py
+++ backup_finder.py
@@ -5,12 +5,37 @@ import plistlib
 import json
 import hashlib
 import os
+import sys
 
 # Deafult Windows itunes backup locations
-paths = [
-    Path(os.getenv("APPDATA")) / "Apple Computer" / "MobileSync" / "Backup",
-    Path.home() / "Apple" / "MobileSync" / "Backup",
-]
+#paths = [
+#    Path(os.getenv("APPDATA")) / "Apple Computer" / "MobileSync" / "Backup",
+#    Path.home() / "Apple" / "MobileSync" / "Backup",
+#]
+
+paths = []
+
+if sys.platform == "win32":
+    os.system("cls")
+    paths = [
+        Path(os.getenv("APPDATA")) / "Apple Computer" / "MobileSync" / "Backup",
+        Path.home() / "Apple" / "MobileSync" / "Backup",
+    ]
+    pass
+
+elif sys.platform == "darwin":
+    os.system("clear")
+    paths = [
+        Path.home() / "Library" / "Application Support" / "MobileSync" / "Backup"
+    ]
+    pass
+
+elif sys.platform.startswith("linux"):
+    print("Linux not supported :/")
+
+else:
+    print("OS not supported!")
+
 
 print("Searching for backups...")
 
@@ -20,7 +45,6 @@ backup_locations = []
 
 for base_path in paths:
     if base_path.exists() and base_path.is_dir():
-        # First check if Info.plist exists in the root directory
         plist_file = base_path / "Info.plist"
         if plist_file.exists():
             i += 1
@@ -36,8 +60,7 @@ Product name: {plist.get("Product Name", "N/A")}
 iOS version: {plist.get("Product Version", "N/A")}
 Backup date: {plist.get("Last Backup Date", "N/A")}
 """)
-        
-        # If not found in root, check all subfolders recursively
+
         for root, dirs, files in os.walk(base_path):
             if "Info.plist" in files:
                 backup_dir = Path(root)
@@ -62,12 +85,9 @@ Backup date: {plist.get("Last Backup Date", "N/A")}
 selected_backup = int(input(f"\nSelect backup: [1-{i}]: "))
 
 
-
-# Get the selected backup location
 selected_path = backup_locations[selected_backup - 1]
 print(f"Backup path: {selected_path}")
 
-# Scan for the specific file
 target_file = selected_path / "39" / "398bc9c2aeeab4cb0c12ada0f52eea12cf14f40b"
 
 if target_file.exists() and target_file.is_file():
@@ -76,7 +96,6 @@ if target_file.exists() and target_file.is_file():
     with open(target_file, "rb") as f:
         plist = plistlib.load(f)
 
-        # Extract keys and values to variables
         restrictions_password_key = plist.get("RestrictionsPasswordKey")
         restrictions_password_salt = plist.get("RestrictionsPasswordSalt")
 
@@ -84,7 +103,7 @@ if target_file.exists() and target_file.is_file():
             print(f"RestrictionsPasswordKey: {restrictions_password_key.hex()}")
             print(f"RestrictionsPasswordSalt: {restrictions_password_salt.hex()}")
 
-            
+
 
         else:
             print("Required keys not found in plist")
